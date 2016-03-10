79bc747ac09b63c316752101af27fc830cf31d07
Clint Beacock <clint.beacock@gmail.com>
2016-03-10 13:27:26 -0500

    Deleted code that was removing and reinstalling rancher-server and rancher-agent-init containers on provision

--- provisioning/scripts/docker/cleanup.sh
+++ provisioning/scripts/docker/cleanup.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 CRASHED=`docker ps -aq -f exited=1`
-RANCHER_SERVER=`docker ps -aq -f name=rancher-server`
-RANCHER_AGENT_INIT=`docker ps -aq -f name=rancher-agent-init`
 
 echo "INFO: Cleaning up any failed containers"
 [ -z "$CRASHED" ] || docker rm -f $CRASHED;
 echo "INFO: Cleanup complete"
-
-
-echo "INFO: Removing rancher-server and agent-init containers for reprovisioning"
-[ -z "$RANCHER_SERVER" ] || docker rm -f $RANCHER_SERVER;
-[ -z "$RANCHER_AGENT_INIT" ] || docker rm -f $RANCHER_AGENT_INIT;
-echo "INFO: Rancher container removal complete"
