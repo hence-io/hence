e962e98e2fa2d4d339880af1b54946e0667bfdd9
Clint Beacock <clint@therefore.ca>
2016-03-21 16:02:14 -0400

    Switched to aufs storage.  Was default before docker 1.9, and the now-default devicemapper storage driver fills up the HD and crashes server

--- provisioning/scripts/docker/configure.sh
+++ provisioning/scripts/docker/configure.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f /etc/default/docker.tcp.forwarded ]
 then
-    echo "DOCKER_OPTS=\"-H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock \${DOCKER_OPTS}\"" >> /etc/default/docker
+    echo "DOCKER_OPTS=\"-H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock --storage-driver=aufs \${DOCKER_OPTS}\"" >> /etc/default/docker
     touch /etc/default/docker.tcp.forwarded
 fi
 
--- provisioning/scripts/system/packages.sh
+++ provisioning/scripts/system/packages.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 apt-get update
-apt-get install -y htop tree lsyncd ncdu
+apt-get install -y htop tree lsyncd ncdu apparmor
+apt-get install -y linux-image-extra-$(uname -r)
